e15c50cccb42056bb7f39a9689e5a85a6cfc1fb3
Nitroceliulioze <aksionova.diana@gmail.com>
2023-09-22 15:20:45 +0300

    docs added

--- storybook-demo/src/Light.stories.tsx
+++ storybook-demo/src/Light.stories.tsx
@@ -4,6 +4,7 @@ import Light from "./Light";
 const meta: Meta<typeof Light> = {
   component: Light,
   title: 'Light',
+  tags: ['autodocs'],
   argTypes: {
     variant: {
         control: {type: 'radio'},
